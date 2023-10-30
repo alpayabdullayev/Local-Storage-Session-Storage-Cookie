e120946ab66b8c0e5823332d9f284c2a414f5096
alpayabdullayev <alpayabdullayev0@gmail.com>
2023-10-30 13:24:16 +0400

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,81 @@
+//local storage
+
+// localStorage.setItem('ad','alpay')
+// localStorage.setItem('soyad','abdullayev')
+
+// let ad = localStorage.getItem('ad')
+// let soyad = localStorage.getItem('soyad')
+
+// console.log(localStorage.length);
+// console.log(localStorage.key[0]);
+// // localStorage.clear() //temiz silmek
+// // localStorage.removeItem('ad') //tek tek silmek
+
+// console.log(ad + " " +soyad);
+
+
+
+//Session storage
+
+// sessionStorage.setItem('username','alpay09')
+// sessionStorage.setItem('eamil','alpayabdullayev0@gmail.com')
+
+
+
+//Cookies
+
+// document.cookie = "username=Alpay; expires=Thu, 18 Oct 2013 12:00:00 UTC; path=/"
+
+
+
+
+//file
+
+// let files = document.getElementById('file')
+
+// files.addEventListener('change',(e)=>{
+//     let file =e.target.files[0]
+
+//     console.log(file.name,file.type,file.size);
+
+// })
+
+
+
+
+//name surname usernarname password localStorage elave olunsun sonra get item p tag icine yazilsin 
+
+// let body = document.querySelector('body')
+
+// const obj = {
+//     name : "ehmed",
+//     surname : "osmanov",
+//     username : "axhmed09",
+//     password : "admin123"
+// }
+
+
+// window.localStorage.setItem("obj", JSON.stringify(obj))
+
+// let newObject = window.localStorage.getItem("obj");
+// let user = JSON.parse(newObject);
+
+// localStorage.setItem('name','alpay')
+// localStorage.setItem('surname','abdullayev')
+// localStorage.setItem('username','alpay09')
+// localStorage.setItem('password','alpay123')
+
+// let name = localStorage.getItem('name')
+// let surname = localStorage.getItem('surname')
+// let username = localStorage.getItem('username')
+// let password = localStorage.getItem('password')
+
+// let p = document.createElement('p')
+
+// p.textContent = `Name: ${name}, Surname: ${surname}, Username: ${username}, Password: ${password}`;
+// p.textContent = `Name: ${user.name}, Surname: ${user.surname}, Username: ${user.username}, Password: ${user.password}`;
+
+// body.append(p)
+
+
+
